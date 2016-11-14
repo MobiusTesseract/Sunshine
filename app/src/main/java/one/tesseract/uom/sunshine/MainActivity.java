1a2751e6c3f086937dee4e647eb16a471e817f1b
MobiusTesseract <MobiusTesseract@users.noreply.github.com>
2016-11-14 02:27:23 +0200

    Minor transition fixes Update dummy weather for today Removed unused string hello world Removed el strings

--- app/src/main/java/one/tesseract/uom/sunshine/MainActivity.java
+++ app/src/main/java/one/tesseract/uom/sunshine/MainActivity.java
@@ -8,9 +8,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.ArrayList;
-import java.util.Arrays;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -61,9 +60,23 @@ public class MainActivity extends ActionBarActivity {
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-            String[] forecastData = {"Yo","Ho","Ho!"};
-            String test = getString(R.string.hello_world);
-            ArrayList<String> forecastList = new ArrayList<>(Arrays.asList(forecastData));
+            String[] weekForecast = {
+                    "Mon 11/14 - Sunny - 31/17",
+                    "Tue 11/15 - Foggy - 21/8",
+                    "Wed 11/16 - Cloudy - 22/17",
+                    "Thurs 11/17 - Rainy - 18/11",
+                    "Fri 11/18 - Foggy - 21/10",
+                    "Sat 11/19 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 11/20 - Sunny - 20/7"
+            };
+            //ArrayList<String> forecastList = new ArrayList<>(Arrays.asList(weekForecast));
+            ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textView,
+                    weekForecast);
+            ListView listView = (ListView) rootView.findViewById(R.id.listView_forecast);
+            listView.setAdapter(mForecastAdapter);
             return rootView;
         }
     }
